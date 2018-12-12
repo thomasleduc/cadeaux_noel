41ae2430e72017c66a4107fc594f4d1f052fbef5
Thomas LEDUC <info.leducthomas@gmail.com>
2018-12-12 16:15:05 +0100

    hide all infos in default.json

--- gifts.js
+++ gifts.js
@@ -3,35 +3,39 @@ Array.prototype.flat = function() {
   return this.reduce((acc, val) => acc.concat(val), []);
 };
 
+const randomIndex = (length, minIndex = 0) => Math.floor(Math.random() * length) + minIndex;
+
+const pickRandomly = arr => arr[randomIndex(arr.length)];
+
 module.exports = (participants, notCompatibles) => {
   // --------
   // helper functions
   // --------
-  const toExclude = giver =>
-    notCompatibles.filter(notCompatible => notCompatible.includes(giver)).flat();
 
-  const randomIndex = (length, minIndex = 0) => Math.floor(Math.random() * length) + minIndex;
+  // return ids of exceptions
+  const toExclude = giverId =>
+    notCompatibles.filter(notCompatible => notCompatible.includes(giverId)).flat();
 
-  const pickRandomly = arr => arr[randomIndex(arr.length)];
-
-  const pickRandomlyWithException = (arr, exceptedValues) => {
-    const arrWithoutException = arr.filter(el => !exceptedValues.includes(el));
-    return pickRandomly(arrWithoutException);
+  // pick randomly a person but not in exceptedValues
+  const pickRandomlyWithException = (particips, exceptedValues) => {
+    const arrWithoutExceptions = particips.filter(({id}) => !exceptedValues.includes(id));
+    return pickRandomly(arrWithoutExceptions);
   };
 
   //---------
   // process
   //---------
-  let receivers = participants.map(participant => participant.name);
+
+  let receivers = [...participants];
 
   return participants.map(giver => {
     // pick in left receivers except the current giver
-    const receiver = pickRandomlyWithException(receivers, [...toExclude(giver.name), giver.name]);
+    const receiver = pickRandomlyWithException(receivers, [...toExclude(giver.id), giver.id]);
 
     // remove receiver from the left receivers
-    receivers = receivers.filter(el => el !== receiver);
+    receivers = receivers.filter(({id}) => id !== receiver.id);
 
-    // return the couple giver/receiver
+    // return the couple giver(object)/receiver(object)
     return { giver, receiver };
   });
 };
--- sms.js
+++ sms.js
@@ -7,20 +7,17 @@ const sms_agent = {
   secret: process.env.NEXMO_SECRET,
 };
 
-module.exports = phones => {
-  const getPhone = id => phones.find(phone => phone.id === id);
-
-  return (giver, receiver) => {
-    const phone = getPhone(giver.id);
+module.exports = phones => (giver, receiver) => {
+    const phone = phones.find(phone => phone.id === giver.id);
     const data = {
       api_key: sms_agent.key,
       api_secret: sms_agent.secret,
       to: phone.number,
-      from: 'Le père Noël',
+      from: 'Santa Berry',
       // prettier-ignore
       text: phone.indirect
-        ? `Salut c'est encore le père Noël pour les petits cadeaux, je peux te charger d'une mission ? Il faut que tu dises à ${giver.name} qu'il doit faire un cadeau à ${receiver}.`
-        : `Salut ${giver.name}, c'est le père Noël pour les petits cadeaux, tu devras offrir un cadeau à ${receiver} de maximum 5 euros qui te fasse penser à l'esprit de Noël.`
+        ? `Salut c'est encore le père Noel pour les petits cadeaux, je peux te charger d'une mission ? Il faut que tu dises à ${giver.name} qu'il doit faire un cadeau à ${receiver.name}.`
+        : `Salut ${giver.name.split(' ')[0]}, la mère Noel s'est trompée, tu devras offrir un cadeau à ${receiver.name} de maximum 10 euros. Bonne chance.`
     };
 
     fetch('https://rest.nexmo.com/sms/json', {
@@ -30,5 +27,4 @@ module.exports = phones => {
     })
       .then(res => res.json())
       .then(json => console.log(json));
-  };
 };
